26ba183dd0e61d5c617ae8cfd7f846f04d54b9aa
YusufAmat <hoccayusuf@gmail.com>
2022-11-15 14:02:20 +0100

    Examples related to OOP have been made.

--- src/oOP/Task1/MainApp.java
+++ src/oOP/Task1/MainApp.java
@@ -1,6 +1,18 @@
 package oOP.Task1;
 
 public class MainApp {
+    /* TODO 1 Create a class named 'Member' having the following members:
+        Data members
+        1 - Name
+        2 - Age
+        3 - Phone number
+        4 - Address
+        5 - Salary
+        It also has a method named 'printSalary' which prints the salary of the members.
+        Two classes 'Employee' and 'Manager' inherits the 'Member' class. The 'Employee' and 'Manager' classes
+        have data members 'specialization' and 'department' respectively.
+        Now, assign name, age, phone number, address and salary to an employee and a manager by making an object
+        of both of these classes and print the same.*/
     public static void main(String[] args) {
         Employee e = new Employee("Ahmet", 28, 52466585,"Erkrath",3000,"Java");
         Manager m = new Manager("Yusuf", 32, 524632585,"Erkrath",5000, "Software");
